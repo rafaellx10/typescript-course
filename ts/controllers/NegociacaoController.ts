5aa2b4bb3f6ad4b0cc9132092e077d8dfa3547f3
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-01-21 10:57:41 -0300

    add negociation to array

--- ts/controllers/NegociacaoController.ts
+++ ts/controllers/NegociacaoController.ts
@@ -2,6 +2,7 @@ class NegociacaoController {
 	private _inputData: HTMLInputElement;
 	private _inputQuantidade: HTMLInputElement;
 	private _inputValor: HTMLInputElement;
+	private _negociacoes = new Negociacoes();
 
 	constructor() {
 		this._inputData = <HTMLInputElement>document.querySelector("#data");
@@ -18,6 +19,12 @@ class NegociacaoController {
 			parseInt(this._inputQuantidade.value),
 			parseFloat(this._inputValor.value)
 		);
-		console.log(negociacao.quantidade + 20);
+		this._negociacoes.adiciona(negociacao);
+
+		this._negociacoes.paraArray().forEach(negociacao => {
+			console.log(negociacao.data);
+			console.log(negociacao.quantidade);
+			console.log(negociacao.valor);
+		});
 	}
 }
