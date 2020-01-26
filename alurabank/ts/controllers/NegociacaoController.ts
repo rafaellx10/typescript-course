c0f97d92ea7d164c9f811c8b44359fa273b460e2
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-01-26 18:30:49 -0300

    isolating the accesse to a API in a Service

--- alurabank/ts/controllers/NegociacaoController.ts
+++ alurabank/ts/controllers/NegociacaoController.ts
@@ -1,6 +1,7 @@
 import { NegociacoesView, MensagemView } from "../views/index";
 import { Negociacoes, Negociacao, NegociacaoParcial } from "../models/index";
 import { domInject, throttle } from "../helpers/decorators/index";
+import { NegociacaoService } from "../service/index";
 
 export class NegociacaoController {
 	@domInject("#data")
@@ -12,6 +13,7 @@ export class NegociacaoController {
 	private _negociacoes = new Negociacoes();
 	private _negociacoesView = new NegociacoesView("#negociacoesView", true);
 	private _mensagemView = new MensagemView("#mensagemView");
+	private _service = new NegociacaoService();
 
 	constructor() {
 		this._negociacoesView.update(this._negociacoes);
@@ -54,25 +56,12 @@ export class NegociacaoController {
 			}
 		}
 
-		fetch("http://localhost:8080/dados")
-			.then(res => isOk(res))
-			.then(res => res.json())
-			.then((dados: NegociacaoParcial[]) => {
-				dados
-					.map(
-						dado =>
-							new Negociacao(
-								new Date(),
-								dado.vezes,
-								dado.montante
-							)
-					)
-					.forEach(negociacao =>
-						this._negociacoes.adiciona(negociacao)
-					);
-				this._negociacoesView.update(this._negociacoes);
-			})
-			.catch(err => console.log(err));
+		this._service.obterNegociacoes(isOk).then(negociacoes => {
+			negociacoes.forEach((negociacao: Negociacao) =>
+				this._negociacoes.adiciona(negociacao)
+			);
+			this._negociacoesView.update(this._negociacoes);
+		});
 	}
 }
 
