fbbbb955b96f8089ac0dccbe2beba6b4c4c4a721
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-01-25 21:05:50 -0300

    learning and using generics

--- ts/views/MensagemView.ts
+++ ts/views/MensagemView.ts
@@ -1,9 +1,5 @@
-class MensagemView extends View {
-	update(model: string) {
-		this._elemento.innerHTML = this.template(model);
-	}
-
-	template(model: string) {
+class MensagemView extends View<string> {
+	template(model: string): string {
 		return `<p class="alert alert-info">${model}</p>`;
 	}
 }
